65f1666035fe3f86ba2dd4db6f9bb81c497c0ba0
PanaderoDelgado <angel.sherpa80@gmail.com>
2026-04-06 14:52:45 +0200

    add benchmar generator

--- benchmark/run_experiments.py
+++ benchmark/run_experiments.py
@@ -1,6 +1,7 @@
 import os
 from .tools import ConfigParser
 from .tools import ExperimentRunner
+from .tools import BenchmarkGenerator
 
 
 if __name__ == "__main__":
@@ -8,5 +9,6 @@ if __name__ == "__main__":
     config_parser = ConfigParser(config_path)
     config = config_parser.config
 
-    runner = ExperimentRunner(config)
-    runner.run()
+    #runner = ExperimentRunner(config)
+    #runner.run()
+    BenchmarkGenerator().save()
